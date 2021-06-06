596bfcfc51a2ce6ea3e89a37dd04f46beba8ccd4
Benjamin Capps <63438571+cappsb@users.noreply.github.com>
2021-06-06 14:30:45 -0500

    Able to point to file and select from dropdown

--- Image-Shrink/main.js
+++ Image-Shrink/main.js
@@ -1,4 +1,10 @@
-const {app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
+const os = require('os')
+const {app, BrowserWindow, Menu, ipcMain, shell} = require('electron')
+// const imagemin = require('imagemin')
+// const imageminMozjpeg = require('imagemin-mozjpeg')
+// const imageminPngquant = require('imagemin-pngquant')
+// const slash = require('slash')
 let mainWindow
 let aboutWindow
 process.env.NODE_ENV = 'development'
@@ -26,13 +32,19 @@ function createMainWindow (){
 
     mainWindow = new BrowserWindow({
         title: 'Image Shrink',
-        width: 500,
+        width: isDev ? 900 : 500,
         height: 600,
-        icon: __dirname+'/assets/icons/profileImage.png',
+        icon: __dirname+'./assets/icons/profileImage.png',
         resizable: isDev,
         backgroundColor:'white',
+        webPreferences:{
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
     })
-    
+    if(isDev){
+        mainWindow.webContents.openDevTools()
+    }
     //mainWindow.loadURL('file://'+__dirname+'/app/index.html')
     mainWindow.loadFile(__dirname+'/app/index.html')
 }
@@ -91,11 +103,39 @@ const menu = [
 ]
 
 
+
+ipcMain.on('image:minimize', (e, options) =>{
+//    options.dest = path.join(os.homedir(), 'imageshrink')
+//    shrinkImage(options)
+    console.log(options)
+})
+
+// async function shrinkImage({ imgPath, quality, dest}){
+//     try{
+//         const pngQuality = quality/100
+//         const files = await imagemin([slash(imgPath)], {
+//             destination: dest,
+//             plugins: [
+//                 imageminMozjpeg({quality}),
+//                 imageminPngquant({
+//                     quality: [pngQuality, pngQuality]
+//                 })
+//             ]
+//         })
+
+//         console.log(files)
+//         shell.openPath(dest)
+
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
   })
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
       app.quit()
